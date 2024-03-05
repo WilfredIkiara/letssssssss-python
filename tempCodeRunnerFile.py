c7449d8dc47c370ef7d9e07c5174f0c9ee680f65
WILFRED IKIARA <133862973+WilfredIkiara@users.noreply.github.com>
2024-03-05 13:18:55 +0300

    Add files via upload

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,3 @@
-#a append
-employee_file = open("employee.txt", "a")
-employee_file.write("toby - Human Recouses")
-print(employee_file.read())
-employee_file.close()
+import useful_tools
+
+print(useful_tools.roll_dice(10))
